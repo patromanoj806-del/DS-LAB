5d55d3722a05d28cc7e059c1d1829948eb869cb3
subhankar patra <patromanoj806@gmail.com>
2026-03-26 23:19:52 +0530

    exp-5 searching&sorting

---
+++ binarysearch.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1, mid;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            return mid; 
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int arr[100], n, key, i, result;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements in sorted order:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Element found at position %d\n", result + 1);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
